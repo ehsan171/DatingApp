059b753836d268b6e410d5851802e65631bf63d9
ehsan171 <ehsanaliasgari@yahoo.com>
2020-04-16 15:08:44 +0430

    Initial Commit 1

---
+++ DatingApp.API/Models/Books.cs
@@ -0,0 +1,11 @@
+namespace DatingApp.API.Models
+{
+    public class Books
+    {
+        
+        public int Id { get; set; }
+        public string Name { get; set; } 
+
+        public string Author { get; set;}
+    }
+}

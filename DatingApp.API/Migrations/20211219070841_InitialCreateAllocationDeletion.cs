6ec2af28e65f8d124ea67a5875ffed11064dbe7b
Ehsan <ehsanaliasgari@yahoo.com>
2021-12-19 13:39:43 +0330

    400-09-28-Deletion is Added

---
+++ DatingApp.API/Migrations/20211219070841_InitialCreateAllocationDeletion.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatingApp.API.Migrations
+{
+    public partial class InitialCreateAllocationDeletion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Allocations",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Allocations");
+        }
+    }
+}

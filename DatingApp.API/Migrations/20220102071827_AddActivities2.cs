e4c80ec5e694d0623dd75b2744567de527c44fdd
Ehsan <ehsanaliasgari@yahoo.com>
2022-01-02 14:06:30 +0330

    400-10-12-Activity Selection is on doing

---
+++ DatingApp.API/Migrations/20220102071827_AddActivities2.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatingApp.API.Migrations
+{
+    public partial class AddActivities2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Activity1",
+                table: "Allocations",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Activity2",
+                table: "Allocations",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Activity3",
+                table: "Allocations",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Activity1",
+                table: "Allocations");
+
+            migrationBuilder.DropColumn(
+                name: "Activity2",
+                table: "Allocations");
+
+            migrationBuilder.DropColumn(
+                name: "Activity3",
+                table: "Allocations");
+        }
+    }
+}

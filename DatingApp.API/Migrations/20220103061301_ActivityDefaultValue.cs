8384bf34e62364c2be3f67c8fd4903abf5603c2b
Ehsan <ehsanaliasgari@yahoo.com>
2022-01-03 14:16:16 +0330

    400-10-13-Selection is on proccess

---
+++ DatingApp.API/Migrations/20220103061301_ActivityDefaultValue.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatingApp.API.Migrations
+{
+    public partial class ActivityDefaultValue : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "FinalAcceptance",
+                table: "Allocations",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "Activity3",
+                table: "Allocations",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "Activity1",
+                table: "Allocations",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "FinalAcceptance",
+                table: "Allocations",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(bool));
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "Activity3",
+                table: "Allocations",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(bool));
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "Activity1",
+                table: "Allocations",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(bool));
+        }
+    }
+}
---
+++ DatingApp.API/Migrations/20220103094904_ActivityDefaultValue2.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatingApp.API.Migrations
+{
+    public partial class ActivityDefaultValue2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "Activity2",
+                table: "Allocations",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "Activity2",
+                table: "Allocations",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(bool));
+        }
+    }
+}

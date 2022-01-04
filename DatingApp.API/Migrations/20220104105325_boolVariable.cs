cf795c9b634999b9734d505b0645bf43bb36f3e0
Ehsan <ehsanaliasgari@yahoo.com>
2022-01-04 14:25:08 +0330

    400-10-14-Action Selection is completed 3

---
+++ DatingApp.API/Migrations/20220104105325_boolVariable.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatingApp.API.Migrations
+{
+    public partial class boolVariable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "FinalAcceptance",
+                table: "Allocations",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "FinalAcceptance",
+                table: "Allocations",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldNullable: true);
+        }
+    }
+}

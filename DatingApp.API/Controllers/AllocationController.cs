942f8f72d88efc6d1e567ccdf319c6a5c0d5c37e
Ehsan <ehsanaliasgari@yahoo.com>
2022-01-12 13:42:49 +0330

    400-10-22

--- DatingApp.API/Controllers/AllocationController.cs
+++ DatingApp.API/Controllers/AllocationController.cs
@@ -395,7 +395,7 @@ namespace DatingApp.API.Controllers
                       
                            
                                 
-                })
+                }).OrderBy(o=>o.Day)
      
                 .ToListAsync();
           
